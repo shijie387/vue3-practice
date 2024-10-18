5f1e17972db4e7cbff08e5ee6edf36d98021178b
shijie liu <108550367+shijie387@users.noreply.github.com>
2024-10-18 11:00:29 +0800

    Add files via upload

---
+++ src-axios/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+import axios from 'axios'
+import {ref,reactive} from 'vue'
+
+let message = reactive({
+  "code":1,
+  "content":"111"
+})
+
+
+async function getMsg2() {
+  let {data} = await getMsg()
+  Object.assign(message,data) 
+}
+
+function getMsg(){
+  let promise = axios({
+    method:"get",
+    url:"https://api.uomg.com/api/rand.qinghua",
+    data:{
+    },
+    params:{
+      format:'json',
+      username:"zhangsan"
+    }
+  })
+
+  promise.then(
+  function(response){
+    Object.assign(message,response.data)
+  }
+)}
+
+</script>
+
+<template>
+  <div>
+    <h1 v-text="message.content"></h1>
+    <button @click="getMsg()">change</button>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
